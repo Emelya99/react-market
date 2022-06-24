2acb26b46e1ea63a51fea983206a617ca3dd311a
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-06-24 22:09:36 +0300

    Do deploy #4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.scss';
 import 'macro-css';
@@ -12,10 +12,10 @@ import { Provider } from 'react-redux';
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>, document.getElementById('root')
 );
