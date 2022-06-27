67ac4166cacb1e3c9a63024d952625718c454edf
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-06-27 21:02:40 +0300

    Added map-section without logic

--- src/components/SectionGeography/index.js
+++ src/components/SectionGeography/index.js
@@ -1,22 +1,75 @@
 import React from 'react';
 import Title from '../Title';
-import './Geography.scss'
+import "./Geography.scss";
+import { defaultTheme } from './Theme';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
+const center = {
+    lat: 50.449607948713826,
+    lng: 30.523178571371687
+};
 
-function Geography() {    
+const defaultOptions = {
+    panControl: true,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    clickableIcons: false,
+    keyboardShortcuts: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    fullscreenControl: false,
+    styles: defaultTheme
+}
 
-    return (
+const Geography = () => {
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: API_KEY
+    })
+
+    const mapRef = React.useRef(undefined);
+
+    const onLoad = React.useCallback(function callback(map) {
+        mapRef.current = map;
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        mapRef.current = undefined;
+    }, [])
+
+    return isLoaded ? (
         <section className="geography sec-bottom">
             <div className="container">
                 <Title
                     title="география"
                 />
-                <p>{API_KEY}</p>
+                <GoogleMap
+                    mapContainerClassName="geography__map"
+                    center={center}
+                    options={defaultOptions}
+                    zoom={14}
+                    onLoad={onLoad}
+                    onUnmount={onUnmount}
+                >
+                    <>
+                        <Marker position={center} icon={{"url":"img/marker.svg"}} />
+                        <div className='geography__content'>
+                            <button className='btn'>юрлица</button>
+                            <button className='btn'>физлица</button>
+                            <button className='btn'>показать всё</button>
+                        </div>
+                    </>
+                </GoogleMap>
             </div>
         </section>
-    );
+
+    ) : <h2>Loading...</h2>
 }
 
 export default Geography;
