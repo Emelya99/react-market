7dc325cd66be531a249ada54295e0dc9417f8d9d
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-06-28 15:51:33 +0300

    Added scroll top on route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import FooterContainer from './components/Footer/FooterContainer';
 import Drawer from './components/Drawer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import {Routes, Route, useLocation} from 'react-router-dom';
+import {useLayoutEffect} from 'react';
 
 const FormContainer = React.lazy(() => import('./components/Form/FormContainer'));
 const Content = React.lazy(() => import('./components/Content'));
@@ -10,14 +12,22 @@ const Content = React.lazy(() => import('./components/Content'));
 function App() {
   const [drawerOpened, setdrawerOpened] = React.useState(false);
 
+  const Wrapper = ({children}) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+  } 
+
   function handleOpenPolitic() {
     setdrawerOpened(true);
-    document.body.style.overflow = "hidden"; // так делать нельзя
+    document.body.style.overflow = "hidden";
   }
 
   function handleClosePolitic() {
     setdrawerOpened(false);
-    document.body.style.overflow = "visible"; // так делать нельзя
+    document.body.style.overflow = "visible";
   }
 
   return (
@@ -25,10 +35,12 @@ function App() {
       <HeaderContainer />
       <main className='main'>
         <React.Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Content />} />
-            <Route path="/form" element={<FormContainer />} />
-          </Routes>
+          <Wrapper>
+            <Routes>
+              <Route path="/" element={<Content />} />
+              <Route path="/form" element={<FormContainer />} />
+            </Routes>
+          </Wrapper>
         </React.Suspense>
       </main>
       <FooterContainer onOpenPolitic={handleOpenPolitic} />
