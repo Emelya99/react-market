de628f3e0e888254ed6b50c52244f77c62cd0bc0
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-06-29 17:48:53 +0300

    Removed scrolToTop function

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import FooterContainer from './components/Footer/FooterContainer';
 import Drawer from './components/Drawer';
 import HeaderContainer from './components/Header/HeaderContainer';
-import {Routes, Route, useLocation} from 'react-router-dom';
-import {useLayoutEffect} from 'react';
+import {Routes, Route} from 'react-router-dom';
 
 const FormContainer = React.lazy(() => import('./components/Form/FormContainer'));
 const Content = React.lazy(() => import('./components/Content'));
@@ -11,15 +10,7 @@ const Content = React.lazy(() => import('./components/Content'));
 function App() {
   const [drawerOpened, setdrawerOpened] = React.useState(false);
 
-  const Wrapper = ({children}) => {
-    const location = useLocation();
-    useLayoutEffect(() => {
-      document.documentElement.scrollTo(0, 0);
-    }, [location.pathname]);
-    return children
-  } 
-
-  function handleOpenPolitic() {
+  function handleOpenPolitic(e) {
     setdrawerOpened(true);
     document.body.style.overflow = "hidden";
   }
@@ -34,12 +25,10 @@ function App() {
       <HeaderContainer />
       <main className='main'>
         <React.Suspense fallback={<div>Loading...</div>}>
-          <Wrapper>
-            <Routes>
-              <Route path="/" element={<Content />} />
-              <Route path="/form" element={<FormContainer />} />
-            </Routes>
-          </Wrapper>
+          <Routes>
+            <Route path="/" element={<Content />} />
+            <Route path="/form" element={<FormContainer />} />
+          </Routes>
         </React.Suspense>
       </main>
       <FooterContainer onOpenPolitic={handleOpenPolitic} />
