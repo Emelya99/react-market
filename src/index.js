56bdbdbdad52e70cc41b7f7a699d6f144179e0c9
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-10-19 22:33:02 +0300

    Fixed deploy bugs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 import 'macro-css';
 
 import App from './App';
-import store from './redux/redux-store'
+import store from './redux/redux-store';
 import { Provider } from 'react-redux';
 
-
 ReactDOM.render(
-    <React.StrictMode>
-        <HashRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </HashRouter>
-    </React.StrictMode>, document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
